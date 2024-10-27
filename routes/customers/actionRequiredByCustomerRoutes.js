f4ecefd5b00a698b58cb1d9d67a7afd9d22d4189
priyanka-chauhan <prchauhan0704@gmail.com>
2024-10-27 22:56:56 +0530

    implemented routes for accept-reject functionalities

--- routes/customers/actionRequiredByCustomerRoutes.js
+++ routes/customers/actionRequiredByCustomerRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const AcceptByWorker = require("../../models/workers/AcceptByWorker");
+const serviceRequests = require("../../models/customers/serviceRequests");
 
 // GET route for fetching accepted requests
 router.get("/fetchAllAcceptedServiceRequests/:customerId", async (req, res) => {
@@ -22,6 +23,39 @@ router.get("/fetchAllAcceptedServiceRequests/:customerId", async (req, res) => {
   } catch (error) {
     console.error(error);
     return res.status(500).json({ msg: "Internal Server Error" });
+  }
+});
+
+//Confirmation by customer
+router.put("/confirmByCustomer/:requestId", async (req, res) => {
+  const { requestId } = req.params; //extract requestId from URL
+  console.log("request id", requestId);
+
+  try {
+    const updatedRequest = await serviceRequests.findByIdAndUpdate(
+      requestId,
+      {
+        status: "Confirmed",
+      },
+      { new: true }
+    ); //update status to confirmed
+
+    if (!updatedRequest) {
+      return res.status(404).json({ message: "Request not found" });
+    }
+
+    // Delete the related document from the AcceptByWorker collection
+    await AcceptByWorker.deleteOne({ service_request_id: requestId });
+
+    res.status(200).json({
+      message: "Request confirmed successfully",
+      updatedRequest,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error occurred while confirming the service request",
+      error,
+    });
   }
 });
 
--- routes/workers/actionRequiredByWorkerRoutes.js
+++ routes/workers/actionRequiredByWorkerRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const acceptByCustomer = require("../../models/customers/acceptByCustomer");
+const JobListing = require("../../models/workers/JobListing");
 
 //GET route for fetching accepted job posts
 router.get("/fetchAllAcceptedJobPostings/:workerId", async (req, res) => {
@@ -19,6 +20,36 @@ router.get("/fetchAllAcceptedJobPostings/:workerId", async (req, res) => {
     console.error(error);
     return res.status(500).json({
       message: "Internal Server Error",
+    });
+  }
+});
+
+//confirmation by worker
+
+router.put("/confirmByWorker/:jobId", async (req, res) => {
+  const { jobId } = req.params; //extract jobId from URL
+
+  try {
+    const updatedJob = await JobListing.findByIdAndUpdate(
+      jobId,
+      {
+        status: "Confirmed",
+      },
+      { new: true }
+    ); //update status to confirmed
+
+    if (!updatedJob) {
+      return res.status(404).json({ message: "Job not found" });
+    }
+
+    res.status(200).json({
+      message: "Job confirmed successfully",
+      updatedJob,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error occurred while confirming the service Job",
+      error,
     });
   }
 });
